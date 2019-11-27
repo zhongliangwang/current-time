baa64cecff2c10f2ec65c4cf67fd81ce501fa91f
Zhongliang Wang <zhongliang.wang@envision-digital.com>
2019-11-27 15:04:08 +0800

    Update get current time

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const core = require("@actions/core");
 const github = require("@actions/github");
+const dayjs = require("dayjs");
 
 try {
-  const time = new Date().toISOString();
+  const time = dayjs().format("YYYYMMDD-HHmmss");
   core.setOutput("time", time);
 } catch (error) {
   core.setFailed(error.message);
